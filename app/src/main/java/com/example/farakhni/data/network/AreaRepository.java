d987d819c237ed4c9625b1efcb59862025b004d7
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2025-05-03 14:27:43 +0300

    Create AreaRepo

---
+++ app/src/main/java/com/example/farakhni/data/network/AreaRepository.java
@@ -0,0 +1,11 @@
+package com.example.farakhni.data.network;
+
+import com.example.farakhni.data.network.NetworkCallBack;
+import com.example.farakhni.model.Area;
+import java.util.List;
+
+public interface AreaRepository {
+    void getAllAreas(NetworkCallBack<List<Area>> callback);
+}
+
+
